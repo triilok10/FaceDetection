9c98a2894441a6d46f8a87fb88660c275e601a61
triilok10 <ittiku3@gmail.com>
2024-12-18 05:00:22 +0530

    Added the Login Controller

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FaceDetection.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
