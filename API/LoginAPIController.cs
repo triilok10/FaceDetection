821e08ee8a78c70d0155b47e7d2ac1ddedad6fd7
TRILOK CHAND SWAMI <ittiku3@gmail.com>
2024-12-22 20:32:33 +0530

    ..

--- API/LoginAPIController.cs
+++ API/LoginAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using FaceDetection.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace FaceDetection.API
 {
@@ -8,6 +9,13 @@ namespace FaceDetection.API
     [ApiController]
     public class LoginAPIController : ControllerBase
     {
+        private readonly string _connectionString;
+
+        public LoginAPIController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("CustomConnection");
+        }
+
 
         [HttpPost]
         public IActionResult LoginAuth([FromBody] LoginCategory pLoginCategory)
@@ -16,7 +24,22 @@ namespace FaceDetection.API
             string message = string.Empty;
             try
             {
+                using (SqlConnection con = new SqlConnection())
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("", con);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("", 1);
+                    cmd.Parameters.AddWithValue("", pLoginCategory.UserCategory);
+                    cmd.Parameters.AddWithValue("", pLoginCategory.UserName);
+                    cmd.Parameters.AddWithValue("", pLoginCategory.UserPassword);
 
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        while (rdr.Read()) { }
+
+                    }
+                }
             }
             catch (Exception ex)
             {
