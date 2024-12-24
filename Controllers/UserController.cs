bd1be04dd5e2bffd11c28343e60aa14239296dd4
TRILOK CHAND SWAMI <ittiku3@gmail.com>
2024-12-24 06:54:49 +0530

    Add the session in the User Controller.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FaceDetection.AppCode;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace FaceDetection.Controllers
 {
@@ -13,6 +15,28 @@ namespace FaceDetection.Controllers
         public IActionResult UserHome()
         {
             return View();
+        }
+    }
+
+    public class SessionAdmin : ActionFilterAttribute
+    {
+        private readonly IClsSession _clsSession;
+
+        public SessionAdmin(IClsSession clsSession)
+        {
+            _clsSession = clsSession;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            int? UserId = _clsSession.GetInt32("UserID");
+            string Username = _clsSession.GetString("UserName");
+            if (string.IsNullOrEmpty(Username) || UserId == null)
+            {
+                _clsSession.SetString("LoginAuth", "Auth");
+                context.Result = new RedirectToActionResult("Login", "Login", null);
+            }
+            base.OnActionExecuting(context);
         }
     }
 }
