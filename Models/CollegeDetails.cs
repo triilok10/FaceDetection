8ecf25a7416b21d9cec1c5cb4b34743fb31e4650
TRILOK CHAND SWAMI <ittiku3@gmail.com>
2024-12-27 06:59:07 +0530

    Working on the Developement section of the CollageDetails

--- Models/CollegeDetails.cs
+++ Models/CollegeDetails.cs
@@ -2,26 +2,30 @@
 {
     public class CollegeDetails
     {
-        //CollageID
-        public int CollageID { get; set; }
-        //CollageName
-        public string CollageName { get; set; }
-        //CollageCode
-        public string CollageCode { get; set; }
-        //CollageCity
-        public string CollageCity { get; set; }
-        //CollageState
-        public string CollageState { get; set; }
-        //CollageCountry
-        public string CollageCountry { get; set; }
-        //CollagePinCode
-        public string CollagePinCode { get; set; }
-        //CollagePhone
-        public string CollagePhone { get; set; }
-        //CollageMail
-        public string CollageMail { get; set; }
-        //IsCollageActive
-        public bool IsCollageActive { get; set; } = false; //Default False
+        //CollegeID
+        public int CollegeID { get; set; }
+        //CollegeName
+        public string CollegeName { get; set; }
+        //CollegeCode
+        public string CollegeCode { get; set; }
+        //CollegeCity
+        public string CollegeCity { get; set; }
+        //CollegeState
+        public string CollegeState { get; set; }
+        //CollegeCountry
+        public string CollegeCountry { get; set; }
+        //CollegePinCode
+        public string CollegePinCode { get; set; }
+        //CollegePhone
+        public string CollegePhone { get; set; }
+        //CollegeMail
+        public string CollegeMail { get; set; }
+        //CollegeAdmin
+        public string CollegeAdmin { get; set; }
+        //CollegeWebsite
+        public string CollegeWebsite { get; set; }
+        //IsCollegeActive
+        public bool IsCollegeActive { get; set; } = false; //Default False
 
     }
 }
