8ba94860fed13ed36d9512064f915ebc276ee532
TRILOK CHAND SWAMI <ittiku3@gmail.com>
2025-01-15 21:45:34 +0530

    ..

--- Models/CollegeDetails.cs
+++ Models/CollegeDetails.cs
@@ -37,7 +37,7 @@
 
         public string? Username { get; set; }
         public string? Password { get; set; }
-        public bool? IsSystemUser { get; set; }
-        public bool? IsActive { get; set; }
+        public bool IsSystemUser { get; set; }
+        public bool IsActive { get; set; }
     }
 }
