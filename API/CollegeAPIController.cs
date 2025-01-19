0720713a1c92a4d5b57e25dcb5ef5377a7b46b99
TRILOK CHAND SWAMI <ittiku3@gmail.com>
2025-01-19 12:37:26 +0530

    ..

---
+++ API/CollegeAPIController.cs
@@ -0,0 +1,59 @@
+﻿using FaceDetection.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace FaceDetection.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CollegeAPIController : ControllerBase
+    {
+
+        #region "Connection String"
+        private readonly string _connectionString;
+
+        public CollegeAPIController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("CustomConnection");
+        }
+        #endregion
+
+        #region "GetRecord CollegeUser"
+        [HttpPost]
+        public CollegeDetails CollegeUser([FromBody] CollegeDetails pCollegeDetails)
+        {
+            CollegeDetails obj = new CollegeDetails();
+
+            try
+            {
+                #region "Validations"
+                if (pCollegeDetails.CollegeID <= 0)
+                {
+                    obj.ErrMsg = "Please select the College.";
+                    obj.Status = false;
+                    return obj;
+                }
+                #endregion
+
+                using (SqlConnection con = new SqlConnection(_connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("usp_CollegeUser", con);
+
+
+
+                }
+            }
+            catch (Exception ex) { }
+
+
+
+            return obj;
+
+        }
+        #endregion
+
+
+    }
+}
--- Controllers/CollegeController.cs
+++ Controllers/CollegeController.cs
@@ -1,18 +1,62 @@
-﻿using FaceDetection.Models;
+﻿using FaceDetection.AppCode;
+using FaceDetection.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace FaceDetection.Controllers
 {
     public class CollegeController : Controller
     {
+
+        #region "Variables and Constructors"
+
+        private readonly HttpClient _httpClient;
+        private readonly dynamic baseUrl;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IClsSession _clsSession;
+        public CollegeController(HttpClient httpClient, IHttpContextAccessor httpContextAccessor, IClsSession clsSession)
+        {
+            _httpClient = httpClient;
+            _clsSession = clsSession;
+            _httpContextAccessor = httpContextAccessor;
+            var request = _httpContextAccessor.HttpContext.Request;
+            baseUrl = $"{request.Scheme}://{request.Host.Value}/";
+            _httpClient.BaseAddress = new Uri(baseUrl);
+        }
+
+        #endregion
+
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
-        public IActionResult CollegeUser(CollegeDetails pCollegeDetails)
+        public async Task<IActionResult> CollegeUser(CollegeDetails pCollegeDetails)
         {
+            CollegeDetails obj = new CollegeDetails();
+            if (pCollegeDetails.CollegeID <= 0)
+            {
+                obj.ErrMsg = "Please select the College";
+                obj.Status = false;
+                return RedirectToAction("", "");
+            }
+
+
+            string APIURL = baseUrl + "api/CollegeAPI/CollegeUser";
+            string Json = JsonConvert.SerializeObject(pCollegeDetails);
+            StringContent con = new StringContent(Json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage res = await _httpClient.PostAsync(APIURL, con);
+            if (res.IsSuccessStatusCode)
+            {
+                dynamic resBody = await res.Content.ReadAsStringAsync();
+                obj = JsonConvert.DeserializeObject<CollegeDetails>(resBody);
+
+            }
+
             return View();
         }
     }
